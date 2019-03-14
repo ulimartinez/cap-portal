830e1fa36cd101674e643509cb00f8c2aa466f05
Ulises <ulimartinez96@gmail.com>
2019-03-14 10:33:16 -0600

    added tooltip

--- src/index.js
+++ src/index.js
@@ -2,15 +2,31 @@ import 'jquery';
 import 'bootstrap';
 import './custom.css';
 import './main.scss';
+import 'popper.js';
 
 const $ = require('jquery');
 
 $(document).ready(function(){
-    let secrets = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    $('[data-toggle="tooltip"]').tooltip();
+    let secrets = [
+        {"code":"udqgrpsdvv", "n":3}, //randompass
+        {"code":"zhsafqvou", "n":7}, //saltyjohn
+        {"code":"qipiiqipii", "n":4}, //meleemelee
+        {"code":"qtttslrfs", "n":5}, //looongman
+        {"code":"ogsgtuuh", "n":6}, //iamanoob
+        {"code":"vqamqopb", "n":8}, //niseight
+        {"code":"ngoqpuswggba", "n":2}, //lemonsqueezy
+        {"code":"kvegmewexm", "n":4}, //graciasati
+        {"code":"olnhdervv", "n":3} //likeaboss
+        ];
 
-    let value = secrets[Math.floor(Math.random() * secrets.length)];
+    let rand = Math.random();
+    let value = secrets[Math.floor(rand * secrets.length)].code;
+    let en = secrets[Math.floor(rand * secrets.length)].n;
+
     $('#secret').text(value);
     $('#encoded').val(value);
+    $('#n').text("n=" + en);
 
     $('form').on('submit', function(e){
         e.preventDefault();
